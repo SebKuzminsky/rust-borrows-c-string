5cd3eb88ad62a7b9c71e5784d918396bfc4c1ba2
Sebastian Kuzminsky <seb@highlab.com>
2023-12-14 20:37:13 -0700

    rust lib: borrow a string from C++

--- rust-borrows-c-string/src/lib.rs
+++ rust-borrows-c-string/src/lib.rs
@@ -1,6 +1,28 @@
-pub fn add(left: usize, right: usize) -> usize {
+use std::ffi::CStr;
+use std::os::raw::c_char;
+
+
+#[no_mangle]
+pub extern "C" fn add(left: usize, right: usize) -> usize {
     left + right
 }
+
+
+#[no_mangle]
+pub extern "C" fn print_string(cptr: *const c_char) {
+    println!("got ptr {:?} from C", cptr);
+
+    let my_cstr: &CStr = unsafe { CStr::from_ptr(cptr) };
+    println!("made a &CStr from it: {:?}", my_cstr);
+    println!("    cstr ptr: {:?}", my_cstr.as_ptr());
+
+    let s: &str = my_cstr.to_str().unwrap();
+    println!("made a &str from it: {:?}", s);
+    println!("    &str ptr: {:?}", s.as_ptr());
+
+    println!("finally here's the string from rust: {}", s);
+}
+
 
 #[cfg(test)]
 mod tests {
